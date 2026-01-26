4d2e3dc577e2127cbb5147b98b210085427707cd
upendtalk <ent-0481@ENT-0481s-MacBook-Air.local>
2026-01-26 18:39:52 +0530

    removed ignore files

--- node_modules/lucide-angular/src/icons/lib/icons.provider.d.ts
+++
@@ -1,5 +0,0 @@
-/** @deprecated Use the injection token LUCIDE_ICONS instead. Will be removed in v1.0. */
-export declare class Icons {
-    private icons;
-    constructor(icons: object);
-}
--- node_modules/lucide-angular/src/icons/lucide-angular-src-icons.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="lucide-angular/src/icons" />
-export * from './public-api';
